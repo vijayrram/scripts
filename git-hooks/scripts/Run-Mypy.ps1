0f7c1af6ba26bc4e746a013c672518a06009deb0
Vijay Ram Raghuraman <vijayrram.ae@gmail.com>
2024-01-30 20:06:50 +0530

    fix: Add Test-Path wrapper to avoid missing folders

--- git-hooks/scripts/Run-Mypy.ps1
+++ git-hooks/scripts/Run-Mypy.ps1
@@ -1,9 +1,16 @@
 Write-Host "Running Mypy checks..."
 
+if (Test-Path "src") {
     Write-Host "Checking Source Files..."
     python -m mypy --strict src
+    if ($LASTEXITCODE) {exit $LASTEXITCODE}
+}
+
+if (Test-Path "tests") {
     Write-Host "Checking Test Files..."
     python -m mypy --strict tests
+    if ($LASTEXITCODE) {exit $LASTEXITCODE}
+}
 
 Write-Host "Mypy checks complete. Exit code: $LASTEXITCODE"
 Write-Host "--------------------------------------------------------------------------------"
--- git-hooks/scripts/Run-Pylint.ps1
+++ git-hooks/scripts/Run-Pylint.ps1
@@ -1,9 +1,16 @@
 Write-Host "Running Pylint checks..."
 
+if (Test-Path "src") {
     Write-Host "Checking Source Files..."
     python -m pylint src
+    if ($LASTEXITCODE) {exit $LASTEXITCODE}
+}
+
+if (Test-Path "tests") {
     Write-Host "Checking Test Files..."
     python -m pylint tests
+    if ($LASTEXITCODE) {exit $LASTEXITCODE}
+}
 
 Write-Host "Pylint checks complete. Exit code: $LASTEXITCODE"
 Write-Host "--------------------------------------------------------------------------------"
