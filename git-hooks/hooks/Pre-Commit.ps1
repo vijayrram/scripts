96e112431ead92d4f4b2fb465e7baec57b7f22ae
Vijay Ram Raghuraman <vijayrram.ae@gmail.com>
2024-01-30 20:03:23 +0530

    fix: Add exits for failed tests to Pre-Commit.ps1

--- git-hooks/hooks/Pre-Commit.ps1
+++ git-hooks/hooks/Pre-Commit.ps1
@@ -1,3 +1,8 @@
 ./git-hooks/scripts/Run-Mypy.ps1
+if ($LASTEXITCODE) {exit $LASTEXITCODE}
+
 ./git-hooks/scripts/Run-Pylint.ps1
+if ($LASTEXITCODE) {exit $LASTEXITCODE}
+
 ./git-hooks/scripts/Run-Pytest.ps1
+if ($LASTEXITCODE) {exit $LASTEXITCODE}
