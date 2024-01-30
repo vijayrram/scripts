736e4def549c7d0c5baa84fe5fee27619b1d4d60
Vijay Ram Raghuraman <vijayrram.ae@gmail.com>
2024-01-30 20:07:22 +0530

    fix: Add exits for failed tests to Run-Pytest.ps1

--- git-hooks/scripts/Run-Pytest.ps1
+++ git-hooks/scripts/Run-Pytest.ps1
@@ -1,13 +1,14 @@
-Write-Host "Running Pylint checks..."
+Write-Host "Running Pytest checks..."
 
     Write-Host "Running unit tests..."
     python -m coverage run -m pytest
+    if ($LASTEXITCODE) {exit $LASTEXITCODE}
 
     Write-Host "Generating reports..."
     python -m coverage report -m
     python -m coverage xml
 
-Write-Host "Pylint checks complete. Exit code: $LASTEXITCODE"
+Write-Host "Pytest checks complete. Exit code: $LASTEXITCODE"
 Write-Host "--------------------------------------------------------------------------------"
 
 exit $LASTEXITCODE
